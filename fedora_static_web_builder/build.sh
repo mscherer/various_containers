37d77865fc5cfebd46b8836ad6219babeabcc9e1
Michael Scherer <misc@redhat.com>
2022-03-25 13:10:08 +0100

    Add a script to build, to be used by woodpecker

---
+++ fedora_static_web_builder/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# config
+#PLUGIN_BUILDER
+#PLUGIN_SERVER
+#PLUGIN_VHOST
+#PLUGIN_USER
+#PLUGIN_SSH_KEY
+#PLUGIN_DESTINATION_DIR
+
+BASE_URL="//${PLUGIN_VHOST}"
+case $PLUGIN_BUILDER in
+	zola)
+		CMD="zola build"
+		CMD_DRAFT="--drafts"
+	       	CMD_BASE_URL="--base-url $BASE_URL"
+		BUILT_SITE="public"
+	;;
+	hugo)
+		CMD="hugo" 
+		CMD_DRAFT="-D"
+		CMD_BASE_URL="--baseURL $BASE_URL"
+		BUILT_SITE="public"
+	;;		
+esac
+
+if [ -z "$PLUGIN_DRAFT" ]; then
+	CMD_DRAFT=""
+fi;
+
+eval $CMD $CMD_DRAFT $CMD_BASE_URL
+
+KEYFILE=/tmp/ssh_key
+echo "$PLUGIN_SSH_KEY" > $KEYFILE
+chmod 700 $KEYFILE
+
+scp -o 'StrictHostKeyChecking no' -r -i $KEYFILE $BUILT_SITE/* $PLUGIN_USER@$PLUGIN_SERVER:$PLUGIN_DESTINATION_DIR
