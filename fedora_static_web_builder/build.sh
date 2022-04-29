a239ff669443a703e96f08f76ad023dcc6b6ef38
Michael Scherer <misc@redhat.com>
2022-04-29 23:00:56 +0200

    Reuse the same trick as project-b website

--- fedora_static_web_builder/build.sh
+++ fedora_static_web_builder/build.sh
@@ -30,7 +30,9 @@ fi;
 eval $CMD $CMD_DRAFT $CMD_BASE_URL
 
 KEYFILE=/tmp/ssh_key
-echo "$PLUGIN_SSH_KEY" > $KEYFILE
+# keep the double $ for variable escaping, and the " for
+# https://stackoverflow.com/questions/22101778/how-to-preserve-line-breaks-when-storing-command-output-to-a-variable
+echo "$${PLUGIN_SSH_KEY}" > $KEYFILE
 chmod 700 $KEYFILE
 
 scp -o 'StrictHostKeyChecking no' -r -i $KEYFILE $BUILT_SITE/* $PLUGIN_USER@$PLUGIN_SERVER:$PLUGIN_DESTINATION_DIR
