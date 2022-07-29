1ca8752e7ecca8a0d849a300685f319243ef788c
Michael Scherer <misc@redhat.com>
2022-07-30 01:34:57 +0200

    Fix zola binary extraction

--- fedora_static_web_builder/download_zola.sh
+++ fedora_static_web_builder/download_zola.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 REPO=getzola/zola
 VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq .name | tr -d '"')
 TMPFILE=/tmp/zola.tar.gz
@@ -7,5 +9,5 @@ echo "Downloading zola $VERSION"
 
 curl -s -L https://github.com/$REPO/releases/download/$VERSION/zola-$VERSION-x86_64-unknown-linux-gnu.tar.gz --output $TMPFILE
 
-tar -xf $TMPFILE /usr/local/bin/zola
+cd /usr/local/bin ; tar -xf $TMPFILE
 rm -f $TMPFILE
