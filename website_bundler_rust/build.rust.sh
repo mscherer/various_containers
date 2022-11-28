a1ee0dfaf8acf2f363872a9b359d3da10c1bf7fb
Michael Scherer <misc@redhat.com>
2022-11-29 00:48:20 +0100

    Add a comment to explain what is going on with the 3 build.sh

--- website_bundler_rust/build.rust.sh
+++ website_bundler_rust/build.rust.sh
@@ -2,6 +2,7 @@ cp /srv/rust-static-server .
 cp /srv/Dockerfile .
 cd rust-static-server
 ln ../public .
+# run the build.sh from the git repository, who compress data
 bash build.sh 
 mv target/x86_64-unknown-linux-gnu/release/static_http ../server
 cd ..
