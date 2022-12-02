700e60701a40004c3669ca55d208924b81134f76
Michael Scherer <misc@redhat.com>
2022-12-02 01:52:14 +0100

    Run the local build.sh

--- website_bundler_rust/build.rust.sh
+++ website_bundler_rust/build.rust.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 cp /srv/rust-static-server .
 cp /srv/Dockerfile .
 cd rust-static-server
 ln ../public .
 # run the build.sh from the git repository, who compress data
-bash build.sh 
+bash ./build.sh
 mv target/x86_64-unknown-linux-gnu/release/static_http ../server
 cd ..
