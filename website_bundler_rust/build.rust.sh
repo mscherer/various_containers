7a6d81f09185d7349a8cf78d3d9f83ef7b69b492
Michael Scherer <misc@redhat.com>
2022-12-02 02:34:59 +0100

    Use a softlink

--- website_bundler_rust/build.rust.sh
+++ website_bundler_rust/build.rust.sh
@@ -2,7 +2,7 @@
 cp -r /srv/rust-static-server .
 cp /srv/Dockerfile .
 cd rust-static-server
-ln ../public .
+ln -s ../public .
 # run the build.sh from the git repository, who compress data
 bash ./build.sh
 mv target/x86_64-unknown-linux-gnu/release/static_http ../server
