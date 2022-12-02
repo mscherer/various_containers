74303586a8b8a390f4496df32f499428e0441589
Michael Scherer <misc@redhat.com>
2022-12-02 18:02:59 +0100

    Do run in background

--- website_busybox/build.local.sh
+++ website_busybox/build.local.sh
@@ -5,6 +5,6 @@ echo "cd /tmp" >> server
 echo "cat << EOF | base64 -d | gzip -d | tar -x -f -" >> server
 ( cd public ; tar -c . -f - | gzip | base64 ) >> server
 echo "EOF" >> server
-echo "busybox httpd -p \${PORT:-8080}" >> server
+echo "busybox httpd -f -p \${PORT:-8080}" >> server
 
 chmod +x server
