08c5e9d4a6c69936185e6e30575791168dbc6dd3
Michael Scherer <misc@redhat.com>
2023-02-23 15:13:37 +0100

    Try again to fix the version

--- fedora_static_web_builder/download_zola.sh
+++ fedora_static_web_builder/download_zola.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-RELEASE=v0.16.1
-
 
 NAME=zola
 REPO=getzola/${NAME}
-VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/$RELEASE | jq .name | tr -d '"')
+VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq .name | tr -d '"')
+VERSION="v0.16.1"
 TARBALL=${NAME}-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
 TMPFILE=/tmp/${NAME}.tar.gz
