26e88776ab5db2516537bb541c25a89bd8969583
Michael Scherer <misc@redhat.com>
2023-02-23 14:58:43 +0100

    Hardcode v0.16.1, due to a bug
    See https://zola.discourse.group/t/error-we-cant-have-a-page-called-index-md-in-the-same-folder-as-an-index-section-in-path-content/1663

--- fedora_static_web_builder/download_zola.sh
+++ fedora_static_web_builder/download_zola.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+RELEASE=v0.16.1
+
+
 NAME=zola
 REPO=getzola/${NAME}
-VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq .name | tr -d '"')
+VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/$RELEASE | jq .name | tr -d '"')
 TARBALL=${NAME}-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
 TMPFILE=/tmp/${NAME}.tar.gz
