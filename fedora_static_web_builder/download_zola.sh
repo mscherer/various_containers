edebeb1a9dfc15261acab1b333d4cb1510c9e164
Michael Scherer <misc@redhat.com>
2023-02-24 22:43:27 +0100

    Reuse the latest, now that 0.17.1 is out

--- fedora_static_web_builder/download_zola.sh
+++ fedora_static_web_builder/download_zola.sh
@@ -5,7 +5,6 @@ set -e
 NAME=zola
 REPO=getzola/${NAME}
 VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq .name | tr -d '"')
-VERSION="v0.16.1"
 TARBALL=${NAME}-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
 
 TMPFILE=/tmp/${NAME}.tar.gz
